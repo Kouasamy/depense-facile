91dbc846b8545f1da1e9afdc3f2b1530967e0ef1
Samuel <kouaekra@gmail.com>
2026-02-04 16:49:09 +0000

    modif des autres pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default defineConfig({
-  root: __dirname,
+  base: '/',
+  root: '.',
   plugins: [
     react(),
     VitePWA({
